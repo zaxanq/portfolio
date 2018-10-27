936f4d021878b689b47676e11deeb6a65b9b072f
Krzysztof <zaxanq@gmail.com>
2018-10-27 22:28:11 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="Stylesheet" type="text/css" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="header-logo">
+            <a class="header-logo-link">
+                Krzysztof<br>
+                Kosmowski
+            </a>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#about-me">/about-me</a></li>
+                <li><a href="#my-skills">/my-skills</a></li>
+            </ul>
+            <ul>
+                <li><a href="#my-works">/my-works</a></li>
+                <li><a href="#contact-me">/contact-me</a></li>
+            </ul>
+        </nav>
+    </header>
+    <div class="hexagon">
+        <div class="hexagon-header">
+            <div class="hexagon-header-description">
+                <span class="description-title">
+                    Junior<br>
+                    Front-end developer<br>
+                </span>
+                <span>
+                    Nazywam się Krzysztof,<br>
+                    moją pasją jest Front-end.<br>
+                    Czerpię satysfakcję widząc efekty<br>
+                    swojej pracy, dlatego chciałbym żeby<br>
+                    to zajęcie zostało moją pracą.
+                </span>
+            </div>
+            <div class="hexagon-header-photo"></div>    
+        </div>
+    </div>
+</body>
+</html>
