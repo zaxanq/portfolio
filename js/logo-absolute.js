941c5f0695b6464d257549e5d418fee51036d53c
Krzysztof <zaxanq@gmail.com>
2018-10-30 22:42:21 +0100

    Small changes concerning new hexagon width

--- js/logo-absolute.js
+++ js/logo-absolute.js
@@ -1,8 +1,9 @@
 var header = $("header");
 var hexagon = $(".hexagon");
 var logo = $(".header-logo");
-var height = 512;
+var height = 448;
 $(window).on("scroll", function(e) {
+  console.log(this.scrollY);
   if (this.scrollY > height) {
     logo.addClass("header-logo__absolute");
   } else if (this.scrollY < height) {
@@ -13,5 +14,5 @@ $(window).on("scroll", function(e) {
 $(document).ready(function(e) {
   header.animate({ height: 1 }, -1).animate({ height: 512 }, 500);
   hexagon.animate({ top: 2000 }, -1).animate({ top: 0 }, 800);
-  logo.animate({ top: -512 }, -1).animate({ top: 0 }, 700);
+  logo.animate({ top: -height }, -1).animate({ top: 0 }, 700);
 });
