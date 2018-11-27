880e9126cded2ef53f94c263e789b052c8c89f68
Krzysztof <zaxanq@gmail.com>
2018-11-27 21:09:45 +0100

    Last changes and finishes in RWD.

--- js/logo.js
+++ js/logo.js
@@ -5,26 +5,30 @@ let scrollLogoTo;
 let scrollHeaderTo;
 let isLandscape = () => $(window).width() >= $(window).height();
 
-$(window).scroll(function () {
-  if (isLandscape()) { // pc (logo is not scrolling on mobile)
+$(window).scroll(function() {
+  if (isLandscape()) {
+    // pc (logo is not scrolling on mobile)
     if ($(window).width() >= 1440) scrollLogoTo = 468;
     else if ($(window).width() >= 1360) scrollLogoTo = 448;
-    else scrollLogoTo = 332;
+    else scrollLogoTo = 420;
   }
 
   if (this.scrollY > scrollLogoTo) logo.addClass("header-logo__absolute");
   else logo.removeClass("header-logo__absolute");
 });
 
-$(document).ready(function () {
+$(document).ready(function() {
   if (isLandscape()) {
     if ($(window).width() >= 1280) scrollHeaderTo = 512;
     else scrollHeaderTo = 408;
-    $(".hexagon").animate({ top: 2000 }, -1).animate({ top: 0 }, 800);
-  }
-  else {
-    scrollHeaderTo = $(window).height();
-    $(".hexagon").animate({ top: 2000 }, -1).animate({ top: 480 }, 800);
+    $(".hexagon")
+      .animate({ top: 2000 }, -1)
+      .animate({ top: 0 }, 800);
+  } else {
+    scrollHeaderTo = "100%";
+    $(".hexagon")
+      .animate({ top: 2000 }, -1)
+      .animate({ top: 480 }, 800);
   }
 
   $("header")
@@ -37,12 +41,12 @@ $(document).ready(function () {
   // the hexagon scrolls up the last
 });
 
-setTimeout(function () {
+setTimeout(function() {
   logo.removeAttr("style");
-  $("header").removeAttr("style");
+  // $("header").removeAttr("style");
   $(".hexagon").removeAttr("style");
 }, 2000);
 
-setTimeout(function () {
+setTimeout(function() {
   $("header").css("transition", ".5s");
 }, 2010);
